9518cb865b866890de69b8f2419628f85f1a82a4
Dinar <dinfat@mail.ru>
2023-01-30 21:42:02 +0300

    Сделал до задачи 5

---
+++ src/Main.java
@@ -0,0 +1,102 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        var dog = 8;
+        var cat = 3.6;
+        var papper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(papper);
+    }
+
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        var dog = 8;
+        dog = dog + 4;
+        var cat = 3.6;
+        cat = cat + 4;
+        var papper = 763789;
+        papper = papper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(papper);
+
+
+    }
+
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        var dog = 8.0;// double dog=8 ??
+        dog = dog - 3.5;
+        var cat = 3.6;
+        cat = cat - 1.6;
+        var papper = 763789;
+        papper = papper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(papper);
+
+
+    }
+
+
+    public static void task4() {
+        System.out.println("Задача 4");
+var friend=19;
+System.out.println(friend);
+friend=friend+2;
+System.out.println(friend);
+friend=friend/7;
+System.out.println(friend);
+
+
+    }
+
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        var frog=3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
+    }
+
+
+    public static void task6() {
+        System.out.println("Задача 6");
+
+
+    }
+
+
+    public static void task7() {
+        System.out.println("Задача 7");
+
+
+    }
+
+
+    public static void task8() {
+        System.out.println("Задача 8");
+
+
+    }
+
+}
